7f6a803835febd8fa21d77fca4a7aebe4523be78
kimInDa <pinktail2327@naver.com>
2023-08-25 22:48:19 +0900

    [Add] 개발 환경 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import './styles/App.css';
+import PokemonImage from './components/PokemonImage';
 
 function App() {
   return (
     <div className="App">
+      <h1>오늘의 포켓몬</h1>
+      <span>오늘 당신의 동료는?</span>
+      <PokemonImage />
     </div>
   );
 }
---
+++ src/components/PokemonImage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PokemonImage() {
+  return <div>PokemonImage</div>;
+}
+
+export default PokemonImage;
